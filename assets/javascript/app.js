e7d2610f672412110da07821d18627ff86a94c8e
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2020-01-09 17:51:31 -0800

    got it to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,183 @@
-// create variables for you stuff
-
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var sumbitButton = document.getElementById('submit');
-
-// create a function to create the quiz
-
-function buildQuiz(){
-  var output = [];
-
-  quizQuestions.forEach(
-  (currentQustion, questionNumber) => {
-     var answer = [];
-
-     for(letter in currentQustion.answers){
-       answers.push(
-         `<label>
-         <input type="radio" name="question${questionNumber}" value="${letter}">
-           ${letter} :
-           ${currentQuestion.answers[letter]}
-           </label>`
-       );
-     }
-
-     output.push(
-       `<div class="question"> ${currentQuestion.question} </div>
-       <div class="answers"> ${answers.join('')} </div>`
-     );
-     }
-  );
+$(document).ready(function(){
   
-  quizContainer.innerHTML = output.join('');
+  // event listeners
+  $("#remaining-time").hide();
+  $("#start").on('click', trivia.startGame);
+  $(document).on('click' , '.option', trivia.guessChecker);
+  
+})
+
+var trivia = {
+  // trivia properties
+  correct: 0,
+  incorrect: 0,
+  unanswered: 0,
+  currentSet: 0,
+  timer: 20,
+  timerOn: false,
+  timerId : '',
+  // questions options and answers data
+  questions: {
+    q1: 'What monster has two horns and lives in the desert regions?',
+    q2: 'Which of these weapons could "not" cut off a monster tail?',
+    q3: 'What item do you get when you combine a potion with honey?',
+    q4: 'What element damage is the Rathian weak against most?',
+    q5: "What item do you use to unburrow a diablos when its started digging after you?",
+    q6: 'What Weapon Has the move Wyvern Fire?',
+    q7: "What Monster can appear in any biome as a threat?"
+  },
+  options: {
+    q1: ['Rathian', 'Diablos', 'Deviljo', 'Barroth'],
+    q2: ['Switch-Axe', 'Heavy-Bowgun', 'Hammer', 'Greatsword'],
+    q3: ['Max-potion', 'Immunizzer', 'Antidote', 'Mega-Potion'],
+    q4: ['Lightning', 'Fire', 'Ice', 'Dragon'],
+    q5: ['Flash Bomb','Sonic Bomb','Dung Bomb','Tranq Bomb'],
+    q6: ['Dual Blades','Gun Lance','Insect Glaive','Bow'],
+    q7: ['Deviljo', 'Rathalos', 'Zinogre','Lagiacrus']
+  },
+  answers: {
+    q1: 'Diablos',
+    q2: 'Hammer',
+    q3: 'Mega-Potion',
+    q4: 'Dragon',
+    q5: 'Sonic Bomb',
+    q6: 'Gun Lance',
+    q7: 'Deviljo'
+  },
+
+  startGame: function(){
+    // restarting game results
+    trivia.currentSet = 0;
+    trivia.correct = 0;
+    trivia.incorrect = 0;
+    trivia.unanswered = 0;
+    clearInterval(trivia.timerId);
+    
+    // show game section
+    $('#game').show();
+    
+    //  empty last results
+    $('#results').html('');
+    
+    // show timer
+    $('#timer').text(trivia.timer);
+    
+    // remove start button
+    $('#start').hide();
+
+    $('#remaining-time').show();
+    
+    // ask first question
+    trivia.nextQuestion();
+    
+  },
+
+  nextQuestion : function(){
+    
+    // set timer to 20 seconds each question
+    trivia.timer = 10;
+     $('#timer').removeClass('last-seconds');
+    $('#timer').text(trivia.timer);
+    
+    // to prevent timer speed up
+    if(!trivia.timerOn){
+      trivia.timerId = setInterval(trivia.timerRunning, 1000);
     }
-  
-
-
-
-function showResults(){}
-
-buildQuiz();
-
-sumbitButton.addEventListener('click', showResults);
-
-// create a array to hold all your questions in.
-
-var quizQuestions = [
-  {
-    question: "What monster has two horns and lives in the desert regions?",
-    answers: {
-      a: "Rathian",
-      b: "Diablos",
-      c: "Deviljo",
-      d: "Barroth"
-    },
-    correctAnswer: "b"
-},
-{
-  question: "Which of these weapons could 'not' cut off a monster tail?",
-  answers: {
-    a: "Switch-Axe",
-    b: "Heavy-Bowgun",
-    c: "Hammer",
-    d: "Greatsword"
+    
+    // gets all the questions then indexes the current questions
+    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    $('#question').text(questionContent);
+    
+    // an array of all the user options for the current question
+    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    
+    // creates all the trivia guess options in the html
+    $.each(questionOptions, function(index, key){
+      $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+    })
+    
   },
-  correctAnswer: "c"
-},
-{
-  question: "What item do you get when you combine a potion with honey?",
-  answers: {
-    a: "Max-potion",
-    b: "Immunizzer",
-    c: "Antidote",
-    d: "Mega-Potion"
+
+  timerRunning : function(){
+    // if timer still has time left and there are still questions left to ask
+    if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+      $('#timer').text(trivia.timer);
+      trivia.timer--;
+        if(trivia.timer === 4){
+          $('#timer').addClass('last-seconds');
+        }
+    }
+    // the time has run out and increment unanswered, run result
+    else if(trivia.timer === -1){
+      trivia.unanswered++;
+      trivia.result = false;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+    }
+    // if all the questions have been shown end the game, show results
+    else if(trivia.currentSet === Object.keys(trivia.questions).length){
+      
+      // adds results of game (correct, incorrect, unanswered) to the page
+      $('#results')
+        .html('<h3>Thank you for playing!</h3>'+
+        '<p>Correct: '+ trivia.correct +'</p>'+
+        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+        '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+        '<p>Please play again!</p>');
+      
+      // hide game sction
+      $('#game').hide();
+      
+      // show start button to begin a new game
+      $('#start').show();
+    }
+    
   },
-  correctAnswer: "d"
-},
-]
+
+  guessChecker : function() {
+    
+    // timer ID for gameResult setTimeout
+    var resultId;
+    
+    // the answer to the current question being asked
+    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    
+    // if the text of the option picked matches the answer of the current question, increment correct
+    if($(this).text() === currentAnswer){
+      // turn button green for correct
+      $(this).addClass('btn-success').removeClass('btn-info');
+      
+      trivia.correct++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Correct Answer!</h3>');
+    }
+    // else the user picked the wrong option, increment incorrect
+    else{
+      // turn button clicked red for incorrect
+      $(this).addClass('btn-danger').removeClass('btn-info');
+      
+      trivia.incorrect++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+    }
+    
+  },
+
+  guessResult : function(){
+    
+    // increment to next question set
+    trivia.currentSet++;
+    
+    // remove the options and results
+    $('.option').remove();
+    $('#results h3').remove();
+    
+    // begin next question
+    trivia.nextQuestion();
+     
+  }
+
+}
